6be534a5264ecbcff64cdb28094c3874e567bbb2
Amol_Narayan_Dhumal <amoldhumal95@gmail.com>
2020-09-10 12:12:41 +0530

    Update Solution.cs

--- Case Study - Sprint 4/Case Study - Sprint 4.1/Solution.cs
+++ Case Study - Sprint 4/Case Study - Sprint 4.1/Solution.cs
@@ -1,26 +1,63 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 
 	//Section A
 	class Customer
 	{
-	    private int cid;
-	    private string cname;
-	    private string password;
-	    private string city;
-	    private string state;
-	    private string zip;
-	    private string country;
-	    public Customer()
-	  {}
-	  public Customer(int a,string b,string c,string d,string e,string f,string g)
-	  {try
+	  //Write your code here 
+	  int cid;
+	  string cname;
+	  string password;
+	  string city;
+	  string state;
+	  string zip;
+	  string country;
+	  //Regex rx = new Regex("^[a-zA-Z]*$", RegexOptions.Compiled);
+
+	  public int Cid
+    	  {  
+    	   get 
+    	  { return cid;}
+    	      set
+    	   {cid=value;  }}
+            public string Cname
+    	  {  
+    	      get 
+    	      { return cname;}
+    	      set{
+    	          //if (cname.Length <6 || !rx.IsMatch(cname)){
+    	            //  cname = "not_valid";
+    	              //throw new InvalidAgeException();}
+    	          //else{
+    	          cname=value;  }}//}
+    	    public string Password
+    	  {  get { return password;}
+    	      set{password=value;  }}
+    	    public string City
+    	  {  get { return city;}
+    	      set{city=value;  }}
+    	    public string State
+    	  {  get { return state;}
+    	      set{state=value;  }}
+    	    public string Zip
+    	  {  get { return zip;}
+    	      set{zip=value;  }}
+    	    public string Country
+    	  {  get { return country;}
+    	      set{country=value;  }
+    	  }
+
+	  public Customer(){
+
+	  }
+	  public Customer(int cid,string cname,string password,string city,string state,string zip,string country){
+	      try
 	  {
-	      if(c.Length<6)
+	      if(cname.Length<6)
 	      {
 	          throw new InvalidNameException("Invalid Name");
 	      }
@@ -30,55 +67,31 @@ using System.Threading.Tasks;
 	          Console.WriteLine("InvalidName");
 	          Console.WriteLine(o.Message);
 	      }
-	  
-	      cid=a;
-	      cname=b;
-	      password=c;
-	      city=d;
-	      state=e;
-	      zip=f;
-	      country=g;
-	      
+	      catch(Exception){return;}
+    	  this.cname = cname; 
+    	  this.cid = cid;
+	      this.password = password;
+	      this.city = city;
+	      this.state = state;
+	      this.zip = zip;
+	      this.country = country;
 	  }
- public int Cid
-	  {  get { return cid;}
-	      set{cid=value;  }}
-public string Cname
-	  {  get { return cname;}
-	      set{cname=value;  }}
-	      public string Password
-	  {  get { return password;}
-	      set{password=value;  }}
-	      public string City
-	  {  get { return city;}
-	      set{city=value;  }}
-	      public string State
-	  {  get { return state;}
-	      set{state=value;  }}
-	      public string Zip
-	  {  get { return zip;}
-	      set{zip=value;  }}
-	      public string Country
-	  {  get { return country;}
-	      set{country=value;  }}
- public override string ToString()
-{
-    string s="CustomerId-"+cid.ToString()+",CustomerName-"+cname+",Password-"+password+",City-"+city+",State-"+state+",Zip-"+zip.ToString()+",Country-"+country;
-Console.Write(s);
-return s;
-}	
-}
+	  public override string ToString(){
+	      return "CustomerId-"+cid+",CustomerName-"+cname+",Password-"+password+",City-"+city+",State-"+state+",Zip-"+zip+",Country-"+country;
+	  }
+	}
 //Section B	
-	class Toy 
+
+        class Toy 
 	{
-	    private int tid;
+	    private int? tid;
 	    private string tname;
 	    private string type;
-	    private int min_age;
-	    private int max_age;
-	    private float price;
-	    private int quantity;
-	    private double rental_amount;
+	    private int? min_age;
+	    private int? max_age;
+	    private float? price;
+	    private int? quantity;
+	    private double? rental_amount;
 	    public Toy()
 	    {}
 	   public  Toy(int h,string i,string j,int k,int l,float m,int o,double n)
@@ -94,8 +107,9 @@ return s;
 	      {
 	          Console.WriteLine("InvalidAgeException");
 	          Console.WriteLine(p.Message);
+	          return;
 	      }
-	  
+	    catch(Exception){return;}
 	      tid=h;
 	      tname=i;
 	      type=j;
@@ -104,9 +118,9 @@ return s;
 	      price=m;
 	      quantity=o;
 	      rental_amount=n;
-	      
+
 	  }
-	   public int Tid
+	   public int? Tid
 	  {  get { return tid;}
 	      set{tid=value;  }}
 public string Tname
@@ -115,38 +129,37 @@ public string Tname
 	      public string Type
 	  {  get { return type;}
 	      set{type=value;  }}
-	      public int Min_age
+	      public int? Min_age
 	  {  get { return min_age;}
 	      set{min_age=value;  }}
-	      public int Max_age
+	      public int? Max_age
 	  {  get { return max_age;}
 	      set{max_age=value;  }}
-	      public float Price
+	      public float? Price
 	  {  get { return price;}
 	      set{price=value;  }}
-	      	      public int Quantity
+	      	      public int? Quantity
 	  {  get { return quantity;}
 	      set{quantity=value;  }}
-	      public double Rental_amount
+	      public double? Rental_amount
 	  {  get { return rental_amount;}
 	      set{rental_amount=value;  }}
-	      public override string ToString()
+	      public override string ToString(){
 
-{
     string s="ToyId-"+tid.ToString()+",ToyName-"+tname+",Type-"+type+",MinAge-"+min_age.ToString()+",MaxAge-"+max_age.ToString()+",Price-"+price.ToString()+",Quantity-"+quantity.ToString()+",RentalAmount-"+rental_amount.ToString();
-Console.Write(s);
+//Console.Write(s);
 return s;
 }
-	//Write your code here  
+}
+	//write your Exception classes implimentations here 
+    public class InvalidNameException: Exception{
+	    public InvalidNameException(){}
+	    public InvalidNameException(string message): base(message) {}
+	    public InvalidNameException(string Message, Exception inner): base(Message, inner) {}
 	}
-	class InvalidNameException: Exception {  
-        public InvalidNameException(string Message): base(Message) {}  
-        public InvalidNameException() {}  
-        public InvalidNameException(string Message, Exception inner): base(Message, inner) {}  
-    }
-    	class InvalidAgeException: Exception {  
+
+	class InvalidAgeException: Exception {  
         public InvalidAgeException(string Message): base(Message) {}  
         public InvalidAgeException() {}  
         public InvalidAgeException(string Message, Exception inner): base(Message, inner) {}  
     }
-	//write your Exception classes implimentations here
